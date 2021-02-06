362ec2d6c7b1ad17f34a670b616133c8da174f32
Ana María Arias <firstana077@gmail.com>
2021-02-06 02:23:27 +0000

    cd

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - principal
+ * Roja-Rob01
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
